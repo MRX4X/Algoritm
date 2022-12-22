0ed01e9ed4c620355f7c1cde6c54aaee1e70ed52
Sharapov Denis <sdr72hoper@gmail.com>
2022-12-22 15:25:11 +0300

    Task: 1029, 530, 1137

---
+++ 4_Task_22.12.22/1.py
@@ -0,0 +1,8 @@
+def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+    costs.sort(key=lambda x: x[0] - x[1])
+    n = len(costs) // 2
+    out = 0
+    for i in costs:
+        n -= 1
+        out += i[1] if n < 0 else i[0]
+    return out
